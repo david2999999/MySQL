6a37c0ea8ee905d62a26af205bb47cb357a1edbc
David Jiang <djiang86@binghamton.edu>
2017-10-16 04:12:55 +0000

    Added Createtable.sql

---
+++ workspace/CreateTable.sql
@@ -0,0 +1,13 @@
+-- // CREATING A TABLE AND DECLARING ITS HEADERS
+-- CREATE TABLE <table name> (
+-- 	Variable <variable type>,
+-- Variable <variable type>,
+-- Variable <variable type>
+-- );
+
+-- //PREVENTS THE USER FROM INSERTING EMPTY VARIABLES INTO TABLE
+-- CREATE TABLE <table name> (
+-- 	Variable <variable type> NOT NULL,
+-- Variable <variable type> NOT NULL,
+-- Variable <variable type> NOT NULL
+-- );
