82cbe7194ad5968d72b0cc71d5858d2010abd085
David Jiang <djiang86@binghamton.edu>
2017-10-16 04:07:52 +0000

    Added Delete.sql

---
+++ workspace/Different Statements/Delete.sql
@@ -0,0 +1,5 @@
+-- DELETING DATA FROM TABLES
+DELETE FROM cats WHERE name=’egg’;
+DELETE FROM cats; 			--deletes all the data but not the table
+DELETE FROM cats WHERE age=14;
+DELETE FROM cats WHERE cat_id=age;
