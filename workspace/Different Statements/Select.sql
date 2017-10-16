51bc601aeaaed1beb04ed3aa2328057693b1d2a8
David Jiang <djiang86@binghamton.edu>
2017-10-16 04:05:42 +0000

    Added select

---
+++ workspace/Different Statements/Select.sql
@@ -0,0 +1,9 @@
+--USING SELECT FOR TABLES
+--SELECT * FROM <table name>;
+--SELECT <column name> FROM <table name>;
+--SELECT <column name> , <column name> FROM <table name>;
+
+SELECT name FROM cats;
+SELECT age, name FROM cats;
+SELECT cat_id FROM cats;
+SELECT name, breed FROM cats;
