ac1d875242289da47c53c6a85b76283863756c5b
David Jiang <djiang86@binghamton.edu>
2017-10-16 04:11:57 +0000

    Added Insert.sql

---
+++ workspace/Different Statements/Insert.sql
@@ -0,0 +1,12 @@
+--INSERTING VALUES INTO TABLES
+-- INSERT INTO <table name>(<column name> ,<column2 name>)
+-- 	VALUES(<column value> , <column2 value>);
+
+-- // INSERTING MULTIPLE VALUES INTO TABLE
+-- INSERT INTO <table name>(<column name> ,<column2 name>)
+-- 	VALUES(<column value> , <column2 value>),
+-- (<column value>, <column2 value>),
+-- (<column value> , <column2 value>);
+
+INSERT INTO cats(age, name)
+	VALUES(12, ‘vicky’);
